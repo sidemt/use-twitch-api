adb0cc221e3e1d386847a2d88ec3a88a26eb5de8
sidemt <25644062+sidemt@users.noreply.github.com>
2018-05-28 06:35:06 +0900

    Implement for loop to fetch information of multiple channels

--- js/index.js
+++ js/index.js
@@ -1,23 +1,36 @@
 $(document).ready(function() {
-  // Send Ajax request
-  $.ajax({
-    url: "https://wind-bow.glitch.me/twitch-api/streams/freecodecamp",
-    data: {
-      callback: ""
-    },
-    type: "GET",
-    dataType: "jsonp",
-    timeout: 10000,
-    // Success handler
-    success: function(data) {
-      console.log(data);
-      $("#results").html(JSON.stringify(data));
-    }, // end success handler
-    error: function() {
-      $("#results").html("An error has occured.");
-    },
-    xhrFields: {
-      withCredentials: false
-    }
-  }); // end ajax
+  var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp"];
+  var results = "";
+
+  for (i = 0; i < usernames.length; i++) {
+    // Send Ajax request
+    $.ajax({
+      url: "https://wind-bow.glitch.me/twitch-api/streams/" + usernames[i],
+      data: {
+        callback: ""
+      },
+      type: "GET",
+      dataType: "jsonp",
+      timeout: 10000,
+      // Success handler
+      success: function(data) {
+        console.log(data);
+        results = results + "<p>" + JSON.stringify(data) + "</p>";
+        console.log(i + "回目：" + results);
+        // $("#results").html(JSON.stringify(data));
+      }, // end success handler
+      error: function() {
+        $("#results").html("An error has occured.");
+      },
+      xhrFields: {
+        withCredentials: false
+      }
+    }); // end ajax
+  } // end for
+
+  // a handler to be called when all Ajax requests have completed
+  $(document).ajaxStop(function() {
+    $("#results").html(results);
+    console.log("RESULTS::: " + results);
+  });
 });
