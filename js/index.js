36528564d0a351b2c721568b64a7411870ec1174
sidemt <25644062+sidemt@users.noreply.github.com>
2018-05-30 11:51:53 +0900

    Get and display required information

--- js/index.js
+++ js/index.js
@@ -1,29 +1,58 @@
 $(document).ready(function() {
   var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp"];
+  var offlineUsers = [];
   var results = "";
+  var channelResults = "";
 
+  // get stream information
   for (i = 0; i < usernames.length; i++) {
     // Send Ajax request
-    $.ajax({
+    $.get({
       url: "https://wind-bow.glitch.me/twitch-api/streams/" + usernames[i],
       data: {
         callback: ""
       },
-      type: "GET",
       dataType: "jsonp",
       timeout: 10000,
       // Success handler
       success: function(data) {
-        console.log(data);
-        results = results + "<p>" + JSON.stringify(data) + "</p>";
-        console.log(i + "回目：" + results);
-        // $("#results").html(JSON.stringify(data));
+        //results = results + "<p>" + JSON.stringify(data) + "</p>";
+
+        if (data.stream === null) {
+          // When Offline
+          $.get({
+            url: data._links.channel.replace(
+              "https://api.twitch.tv/kraken/channels/",
+              "https://wind-bow.glitch.me/twitch-api/channels/"
+            ),
+            data: {
+              callback: ""
+            },
+            dataType: "jsonp",
+            timeout: 10000,
+            // Success handler
+            success: function(data) {
+              results =
+                results + createPanel(data.display_name, data.logo, data.url, "Offline");
+            }, // end success handler
+            error: function() {
+              results = results + "<p> Could not retreave channel data</p>";
+            }
+          }); // end ajax
+        } else {
+          // When Online
+          results =
+            results +
+            createPanel(
+              data.stream.channel.display_name,
+              data.stream.channel.logo,
+            data.stream.channel.url,
+              data.stream.channel.status
+            );
+        } // end if
       }, // end success handler
       error: function() {
         $("#results").html("An error has occured.");
-      },
-      xhrFields: {
-        withCredentials: false
       }
     }); // end ajax
   } // end for
@@ -31,6 +60,25 @@ $(document).ready(function() {
   // a handler to be called when all Ajax requests have completed
   $(document).ajaxStop(function() {
     $("#results").html(results);
-    console.log("RESULTS::: " + results);
+    console.log("Offline Users: " + offlineUsers);
+    // $("#channels").html(channelResults);
   });
+
+  // HTMLに成型して出力する関数
+  function createPanel(displayName, logo, link, status) {
+    var html =
+      "<div>" +
+      "<a href=\"" + link + "\" target=\"_blank\">" +
+      displayName +
+      "</a>" +
+      '<img src="' +
+      logo +
+      '">' +
+      "<p>" +
+      status +
+      "</p>" +
+      "</div>";
+
+    return html;
+  }
 });
